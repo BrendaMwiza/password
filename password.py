55a53edbe199a22c12f94d1499b07db983170089
BrendaMwiza <brendabrizy@gmail.com>
2019-09-05 11:23:57 +0200

    created a class to hold users

---
+++ password.py
@@ -0,0 +1,20 @@
+class Ingufuri:
+    '''
+    class that will generate new user-names,emails and passwords
+    '''
+    def __init__(self,full_name,email,password):
+
+        """
+        This _init_ method will help us define our object properties.
+
+        arge:
+            full_name: New user full name.
+            email: New user amail address.
+            password: New user password.
+        """
+
+        self.full_name = full_name
+        self.email = email
+        self.pasword = pasword
+
+        
